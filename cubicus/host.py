15c1121a34a45d029c272a524f4871dd453ed964
James Potter <james.potter@gmail.com>
2012-02-27 23:25:32 +0000

    App thread listens for app/context changes and notifies app

--- cubicus/host.py
+++ cubicus/host.py
@@ -8,8 +8,19 @@ class ApplicationSocketThread(SocketThread):
     """
     Accepts a socket to a local Cubicus-enabled application and services it
     """
-    #def __init__(self, clientsocket):
-        #SocketThread.__init__(self, clientsocket)
+    def __init__(self, clientsocket):
+        SocketThread.__init__(self, clientsocket)
+        self._app = None
+        # Subscribe to manager updates
+        self.manager.subscribe(self)
+
+    def notify(self, name, new_value):
+        if name in ['current_application', 'current_context'] and self._app:
+            # Notify app of changed context if we're the currently
+            # active application ID
+            man = self.manager
+            if man.current_application == self._app.application_id:
+                self.queue_message('switch_context', man.current_context)
 
     def allowed_types(self):
         types = ['application_identify', 'switch_context']
@@ -18,6 +29,7 @@ class ApplicationSocketThread(SocketThread):
     def handle_application_identify(self, json_app):
         app = Application.from_json(json_app)
         self.manager.add_application(app)
+        self._app = app
 
     def handle_switch_context(self, context_id):
         self.manager.current_context = context_id
